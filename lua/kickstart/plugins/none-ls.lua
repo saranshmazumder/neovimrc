c45b0651d85d1933e3ab8aa723d7f9a584adbf74
kepplertreet <saranshmazumder98@gmail.com>
2024-08-06 18:59:15 +0530

    vimsetup

---
+++ lua/kickstart/plugins/none-ls.lua
@@ -0,0 +1,22 @@
+return {
+  'nvimtools/none-ls.nvim',
+  dependencies = {
+    'nvimtools/none-ls-extras.nvim',
+  },
+  config = function()
+    local null_ls = require 'null-ls'
+    null_ls.setup {
+      sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.black,
+        null_ls.builtins.formatting.prettier,
+        require 'none-ls.diagnostics.ruff',
+      },
+    }
+
+    -- Set keymap for formatting
+    vim.keymap.set('n', '<leader>gf', function()
+      vim.lsp.buf.format { async = true }
+    end, { desc = '[G]lobal [F]ormat' })
+  end,
+}

4875a33655a9aa3a1493977d46a0672c149ec620
saranshmazumder <154113857+saranshmazumder@users.noreply.github.com>
2024-08-08 15:21:18 +0530

    broke my config

--- lua/colourmyscreen.lua
+++ lua/colourmyscreen.lua
@@ -20,48 +20,19 @@ return {
       },
     },
     config = function()
-      ColorMyPencils()
+      ColorMyPencils 'vague'
     end,
   },
-  {
-    'ellisonleao/gruvbox.nvim',
-    name = 'gruvbox',
-    config = function()
-      require('gruvbox').setup {
-        terminal_colors = true, -- add neovim terminal colors
-        undercurl = true,
-        underline = false,
-        bold = true,
-        italic = {
-          strings = false,
-          emphasis = false,
-          comments = false,
-          operators = false,
-          folds = false,
-        },
-        strikethrough = true,
-        invert_selection = false,
-        invert_signs = false,
-        invert_tabline = false,
-        invert_intend_guides = false,
-        inverse = true, -- invert background for search, diffs, statuslines and errors
-        contrast = '', -- can be "hard", "soft" or empty string
-        palette_overrides = {},
-        overrides = {},
-        dim_inactive = false,
-        transparent_mode = false,
-      }
-    end,
-  },
+
   {
     'rose-pine/neovim',
     name = 'rose-pine',
     config = function()
       require('rose-pine').setup {
-        disable_background = true,
         styles = {
           italic = false,
           bold = true,
+          transparency = true,
         },
       }
     end,
